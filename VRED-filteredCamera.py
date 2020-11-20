1d688b47f036cae5e768a3e71e35ba2712e121f9
Simon Nagel <61537975+simonnagel@users.noreply.github.com>
2020-11-20 08:45:50 +0100

    Update VRED-filteredCamera.py

--- VRED-filteredCamera.py
+++ VRED-filteredCamera.py
@@ -1,4 +1,12 @@
 '''
+DISCLAIMER:
+---------------------------------
+In any case, all binaries, configuration code, templates and snippets of this solution are of "work in progress" character.
+This also applies to GitHub "Release" versions.
+Neither Simon Nagel, nor Autodesk represents that these samples are reliable, accurate, complete, or otherwise valid. 
+Accordingly, those configuration samples are provided ?as is? with no warranty of any kind and you use the applications at your own risk.
+
+
 Scripted by Simon Nagel
 Use this Script in a collaboration session to filter/smooth the jittering camera movement of a HMD user. The image appear less shaky for the spectators.
 
